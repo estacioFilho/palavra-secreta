3427187ecf19b1c0899538230d63493ec8788ff2
estacioFilho <estacioqueirozfilho@gmail.com>
2024-11-14 14:46:22 -0300

    word_secret/stage_game

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//css
 import './App.css'
+//data
+import { category } from './data/data.js'
+//hooks
+import { useCallback, useState, useEffect } from 'react';
+//components
+import StartScreen from './components/startScreen/Index.jsx'
+import Game from './components/game/Index.jsx';
+import GameOver from './components/gameOver/Index.jsx';
+
+const stage = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [gameStage, setGameStage] = useState(stage[0].name);
+  const [wordList] = useState(category);
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [latters, setLatters] = useState([]);
+  
+
+  const [guessedLatters, setGuessedLatters] = useState([]);
+  const [wrongLatters, setWrongLatters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+  console.log(wrongLatters)
+  const pickedWordAndCategory = () => {
+    const categories = Object.keys(wordList)
+    const randomCategory = categories[Math.floor(Math.random() * categories.length)]
+
+    const randomWord = wordList[randomCategory][Math.floor(Math.random() * wordList[randomCategory].length)]
+    return { randomCategory, randomWord };
+  }
+
+
+  const startGame = () => {
+    const { randomCategory, randomWord } = pickedWordAndCategory();
+
+
+    let wordLatter = randomWord.split("")
+    wordLatter = wordLatter.map((l) => l.toLowerCase())
+   
+
+    setPickedCategory(randomCategory);
+    setPickedWord(randomWord);
+    setLatters(wordLatter);
+
+
+
+
+    setGameStage(stage[1].name)
+  }
+
+  const verifyLetter = (letter) =>{
+    const normalizeLetter = letter.toLowerCase();
+    if(guessedLatters.includes(letter) || wrongLatters.includes(letter)){
+      return;
+    }
+    
+    if(latters.includes(normalizeLetter)){
+      setGuessedLatters((actualGuessedLetters)=>[
+        ...actualGuessedLetters,
+        normalizeLetter
+        
+      ])
+      console.log(`Normalize: ${normalizeLetter}`)
+    }else{
+      setWrongLatters((actualWrongLetters)=>[
+        ...actualWrongLetters,
+        normalizeLetter
+      ])
+    }
+  }
+  
+  
+  const gameOver = () => {
+    setGameStage(stage[2].name)
+  }
+
+  const retry = () => {
+    setGameStage(stage[0].name)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      {gameStage == 'start' && <StartScreen startGame={startGame} />}
+      {gameStage == 'game' && (<Game
+        gameOver={gameOver}
+        pickedCategory={pickedCategory}
+        pickedWord={pickedWord}
+        latters={latters}
+        guessedLatters={guessedLatters}
+        wrongLatters={wrongLatters}
+        guesses={guesses}
+        score={score}
+        verifyLetter={verifyLetter}
+      />
+      )}
+      {gameStage == 'end' && <GameOver retry={retry} />}
+
+    </div>
   )
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+export const category = {
+    frutas: ['maçã', 'banana', 'laranja', 'abacaxi', 'uva', 'manga', 'morango'],
+    animais: ['gato', 'cachorro', 'elefante', 'leão', 'tigre', 'pássaro', 'girafa'],
+    cores: ['vermelho', 'azul', 'verde', 'amarelo', 'preto', 'branco', 'rosa'],
+    profissões: ['engenheiro', 'médico', 'advogado', 'professor', 'designer', 'arquiteto', 'enfermeiro'],
+    objetos: ['cadeira', 'mesa', 'telefone', 'relógio', 'computador', 'livro', 'lâmpada'],
+    países: ['Brasil', 'Estados Unidos', 'França', 'Alemanha', 'Japão', 'China', 'Itália']
+  };
+  
+
+  
