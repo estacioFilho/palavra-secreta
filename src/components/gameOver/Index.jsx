d19c3ff55329169e4c3e53de68513edaabab64ba
estacioFilho <estacioqueirozfilho@gmail.com>
2024-11-20 15:35:19 -0300

    Word secrect end of project

--- src/components/gameOver/Index.jsx
+++ src/components/gameOver/Index.jsx
@@ -1,9 +1,10 @@
 import './Style.css'
 
-const GameOver = ({retry}) =>{
+const GameOver = ({retry, score}) =>{
     return(
         <div className='retry'>
             <h1>Fim de jogo</h1>
+            <h2>Sua potuação foi: <span>{score}</span></h2>
             <button onClick={retry}>Reiniciar jogo</button>
         </div>
     )
